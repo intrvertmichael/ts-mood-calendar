848c6517f64b61b0ef182804bdb6c3ad643b2b5f
intrvertmichael <michaelpaguay@gmail.com>
2020-06-05 17:37:25 -0400

    made login page

--- src/components/LogInPage.tsx
+++ src/components/LogInPage.tsx
@@ -1,32 +1,36 @@
 import React from 'react';
-import {LogIn} from '../redux/actions/firebaseActions';
-import {connect} from 'react-redux';
-import {Dispatch} from 'redux';
-import './../syles/LogInPage.css'
+import { LogIn } from '../redux/actions/firebaseActions';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
+import './../syles/LogInPage.css';
+import GoogleButton from 'react-google-button';
 
-const LogInPage = (props:any) =>{
-  return (
-    <div className='login'>
-      <div className='login-message'>
-        log in page
-      </div>
-      <button  className='login-btn' onClick={()=>props.LogIn()}>Log In</button>
-    </div>
-  )
-}
+const LogInPage = (props: any) => {
+	return (
+		<div className='login'>
+			<div className='login-message'>
+				<h1>Mood Calendar</h1>
+				<p>Keep track of your mood and what's going on.</p>
+			</div>
+			<div className='login-btn'>
+				<GoogleButton type='light' onClick={() => props.LogIn()} />
+			</div>
+		</div>
+	);
+};
 
-const mapStateToProps = (state:any) => {
-  return {
-    calendar: state,
-    current: state.current.modalOpen,
-    loggedIn: state.firebase.auth.uid
-  }
-}
+const mapStateToProps = (state: any) => {
+	return {
+		calendar: state,
+		current: state.current.modalOpen,
+		loggedIn: state.firebase.auth.uid,
+	};
+};
 
-const mapDispatchToProps= (dispatch:Dispatch) => {
-  return {
-    LogIn: () => dispatch(LogIn())
-   }
-}
+const mapDispatchToProps = (dispatch: Dispatch) => {
+	return {
+		LogIn: () => dispatch(LogIn()),
+	};
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(LogInPage);
