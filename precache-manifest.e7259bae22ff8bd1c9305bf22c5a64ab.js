c3afc18a03bb5816aa723ae37455188978aca0d6
intrvertmichael <michaelpaguay@gmail.com>
2020-06-08 15:29:11 -0400

    Updates

---
+++ precache-manifest.e7259bae22ff8bd1c9305bf22c5a64ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8ee3996df616eb299e96a5cacb43de4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b140c7c49bbf5fac1b15",
+    "url": "/static/css/main.38277488.chunk.css"
+  },
+  {
+    "revision": "e15dd1bbf0a346c6cc9a",
+    "url": "/static/js/2.fcd25713.chunk.js"
+  },
+  {
+    "revision": "563d5f98b5157ab93e93caae55951c90",
+    "url": "/static/js/2.fcd25713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b140c7c49bbf5fac1b15",
+    "url": "/static/js/main.a508b4d8.chunk.js"
+  },
+  {
+    "revision": "d7e58a5cb5ee6b3d0b1c",
+    "url": "/static/js/runtime-main.f67e39f7.js"
+  }
+]);
